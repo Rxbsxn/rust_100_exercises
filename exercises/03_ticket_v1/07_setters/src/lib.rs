f44f6c241d9b7930346c7ea563917c192b4f3ecd
Robert Harężlak <harezlak.robert7@gmail.com>
2024-10-12 08:27:28 +0200

    feat: add solution to 03/07

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -10,22 +10,39 @@ pub struct Ticket {
 }
 
 impl Ticket {
-    pub fn new(title: String, description: String, status: String) -> Ticket {
+    fn is_title_valid(title: &String) -> bool {
         if title.is_empty() {
             panic!("Title cannot be empty");
         }
+
         if title.len() > 50 {
             panic!("Title cannot be longer than 50 bytes");
         }
+
+        true
+    }
+
+    fn is_description_valid(description: &String) -> bool {
         if description.is_empty() {
             panic!("Description cannot be empty");
         }
         if description.len() > 500 {
             panic!("Description cannot be longer than 500 bytes");
         }
+
+        true
+    }
+
+    fn is_status_valid(status: &String) -> bool {
         if status != "To-Do" && status != "In Progress" && status != "Done" {
             panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
         }
+        true
+    }
+    pub fn new(title: String, description: String, status: String) -> Ticket {
+        Self::is_title_valid(&title);
+        Self::is_description_valid(&description);
+        Self::is_status_valid(&status);
 
         Ticket {
             title,
@@ -38,12 +55,30 @@ impl Ticket {
         &self.title
     }
 
+    pub fn set_title(&mut self, new_title: String) -> () { 
+        Self::is_title_valid(&new_title);
+
+        self.title = new_title;
+    }
+
     pub fn description(&self) -> &String {
         &self.description
     }
 
+    pub fn set_description(&mut self, new_description: String) -> () { 
+        Self::is_description_valid(&new_description);
+
+        self.description = new_description;
+    }
+
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    pub fn set_status(&mut self, new_status: String) -> () { 
+        Self::is_status_valid(&new_status);
+
+        self.status = new_status;
     }
 }
 
